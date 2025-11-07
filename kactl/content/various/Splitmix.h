f6810d10c64ba5446f851564b92fe5a6282b2738
andreyDagger <88271120+andreyDagger@users.noreply.github.com>
2025-11-07 13:10:19 +0300

    Create Splitmix.h

---
+++ kactl/content/various/Splitmix.h
@@ -0,0 +1,20 @@
+/**
+ * Author: Neal Wu
+ * Date: 2025-11
+ * License: CC0
+ * Source: https://codeforces.com/blog/entry/62393
+ * Description: uint64_t hasher
+ */
+
+struct custom_hash {
+    static uint64_t splitmix64(uint64_t x) {
+        x += 0x9e3779b97f4a7c15;
+        x = (x ^ (x >> 30)) * 0xbf58476d1ce4e5b9;
+        x = (x ^ (x >> 27)) * 0x94d049bb133111eb;
+        return x ^ (x >> 31);
+    }
+    size_t operator()(uint64_t x) const {
+        static const uint64_t FIXED_RANDOM = chrono::steady_clock::now().time_since_epoch().count();
+        return splitmix64(x + FIXED_RANDOM);
+    }
+};
